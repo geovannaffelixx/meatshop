957de2bddcbabb5a038840e88a441d1a285d7b99
RafaelMartins8 <rafinha.francamartins@outlook.com>
2025-10-25 17:05:56 -0300

    ci(test): update jest config to include e2e tests

--- meatshop-backend/jest.config.cjs
+++ meatshop-backend/jest.config.cjs
@@ -1,9 +1,13 @@
-// meatshop-backend/jest.config.cjs
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
-  testMatch: ['**/*.spec.ts'],        
-  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  testMatch: [
+    '**/test/**/*.e2e-spec.ts',
+    '**/*.spec.ts',
+  ],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
   collectCoverageFrom: ['src/**/*.(t|j)s'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
