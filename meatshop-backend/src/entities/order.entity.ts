8eeb0135d8768310b48baf0f7c256b5bb355f975
RafaelMartins8 <rafinha.francamartins@outlook.com>
2025-11-03 12:40:20 -0300

    transfer

--- meatshop-backend/src/entities/order.entity.ts
+++ meatshop-backend/src/entities/order.entity.ts
@@ -1,13 +1,34 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
-import { PaymentMethod } from './expense.entity';
+
+// 🔹 Define o ENUM dentro da própria entidade
+export enum PaymentMethod {
+  PIX = 'Pix',
+  CREDITO = 'Crédito',
+  DEBITO = 'Débito',
+  DINHEIRO = 'Dinheiro',
+}
+
 @Entity('orders')
 export class Order {
-  @PrimaryGeneratedColumn() id: number;
-  @Column() cliente: string;
-  @Column({ default: 'Pendente' }) status: string;
-  @Column('decimal', { precision: 10, scale: 2, default: 0 }) valor: number;
-  @Column({ type: 'varchar', length: 50, nullable: true })
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  cliente: string;
+
+  @Column({ default: 'Pendente' })
+  status: string;
+
+  @Column('decimal', { precision: 10, scale: 2, default: 0 })
+  valor: number;
+
+  @Column({
+    type: 'enum',
+    enum: PaymentMethod,
+    nullable: true,
+  })
   paymentMethod: PaymentMethod;
 
-  @CreateDateColumn() criadoEm: Date;
+  @CreateDateColumn()
+  criadoEm: Date;
 }
