d7c45f71ef087efd46ad23fc9eddb5df5d4495e8
RafaelMartins8 <rafinha.francamartins@outlook.com>
2025-11-03 17:50:22 -0300

    transfer

--- meatshop-backend/src/migrations/1762182963209-AddPaymentMethodToOrders.ts
+++ meatshop-backend/src/migrations/1762182963209-AddPaymentMethodToOrders.ts
@@ -1,16 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddPaymentMethodToOrders1762182963209 implements MigrationInterface {
-    name = 'AddPaymentMethodToOrders1762182963209'
+  name = 'AddPaymentMethodToOrders1762182963209';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" ADD "paymentMethod" character varying(50)`);
-        await queryRunner.query(`ALTER TABLE "expenses" ALTER COLUMN "paidAmount" SET DEFAULT '0'`);
-    }
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "orders" ADD "paymentMethod" character varying(50)`);
+    await queryRunner.query(`ALTER TABLE "expenses" ALTER COLUMN "paidAmount" SET DEFAULT '0'`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "expenses" ALTER COLUMN "paidAmount" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "paymentMethod"`);
-    }
-
+  async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "expenses" ALTER COLUMN "paidAmount" DROP DEFAULT`);
+    await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "paymentMethod"`);
+  }
 }
--- meatshop-backend/src/migrations/1762184229240-AddPaymentEnumToOrders.ts
+++ meatshop-backend/src/migrations/1762184229240-AddPaymentEnumToOrders.ts
@@ -1,18 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddPaymentEnumToOrders1762184229240 implements MigrationInterface {
-    name = 'AddPaymentEnumToOrders1762184229240'
+  name = 'AddPaymentEnumToOrders1762184229240';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "paymentMethod"`);
-        await queryRunner.query(`CREATE TYPE "public"."orders_paymentmethod_enum" AS ENUM('Pix', 'Crédito', 'Débito', 'Dinheiro', 'Boleto')`);
-        await queryRunner.query(`ALTER TABLE "orders" ADD "paymentMethod" "public"."orders_paymentmethod_enum"`);
-    }
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "paymentMethod"`);
+    await queryRunner.query(
+      `CREATE TYPE "public"."orders_paymentmethod_enum" AS ENUM('Pix', 'Crédito', 'Débito', 'Dinheiro', 'Boleto')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "orders" ADD "paymentMethod" "public"."orders_paymentmethod_enum"`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "paymentMethod"`);
-        await queryRunner.query(`DROP TYPE "public"."orders_paymentmethod_enum"`);
-        await queryRunner.query(`ALTER TABLE "orders" ADD "paymentMethod" character varying(50)`);
-    }
-
+  async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "paymentMethod"`);
+    await queryRunner.query(`DROP TYPE "public"."orders_paymentmethod_enum"`);
+    await queryRunner.query(`ALTER TABLE "orders" ADD "paymentMethod" character varying(50)`);
+  }
 }
