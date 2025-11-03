b11122408d0915b2a23f099bf7267e0d2b800d26
RafaelMartins8 <rafinha.francamartins@outlook.com>
2025-11-03 18:05:19 -0300

    transfer

--- meatshop-backend/jest.config.cjs
+++ meatshop-backend/jest.config.cjs
@@ -1,13 +1,21 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
-  testMatch: [
-    '**/test/**/*.e2e-spec.ts',
-    '**/*.spec.ts',
-  ],
-  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
-  collectCoverageFrom: ['src/**/*.(t|j)s'],
-  coverageDirectory: 'coverage',
+  testRegex: '.*\\.e2e-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        isolatedModules: false,
+      },
+    ],
+  },
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: '../coverage',
   testEnvironment: 'node',
-  transformIgnorePatterns: ['<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
